e867fc45be20e9e8f001b3d6ff2e83d3ecb10369
nmaddp1995 <nmaddp199599@gmail.com>
2017-04-04 14:09:10 +0700

    initial commit

--- server.js
+++ server.js
@@ -5,7 +5,18 @@ var port = process.env.PORT || 8080;
 
 
 app.get('/',function(req,res){
-	res.send("Server ok");
+	var browserInfo = req.headers['user-agent'];
+	var browser = req.headers['user-agent'].split("(")[1].split(")")[0];
+	var ip;
+getClientAddress = function (req) {
+        return (req.headers['x-forwarded-for'] || '').split(',')[0] 
+        || req.connection.remoteAddress;
+};
+	ip=getClientAddress(req);
+	var info = {browser: browser};
+	
+	res.send(ip);
+	// res.json(info);
 
 });
 
